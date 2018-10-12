a99972f6a61160b02b499667acc3867789ad0136
CJ <cmlong3@buffalo.edu>
2018-10-11 21:48:00 -0400

    Added a favorite button to Alumni Screen, doesn't do anything other than toggle colors yet

--- app/src/main/java/com/example/ubathletics/AlumniGymFragment.java
+++ app/src/main/java/com/example/ubathletics/AlumniGymFragment.java
@@ -6,12 +6,35 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
-public class AlumniGymFragment extends Fragment{            //Sets up super basic fragment
+public class AlumniGymFragment extends Fragment {            //Sets up super basic fragment
+    View inflatedView = null;
+    ImageButton favoriteButton = null;
     @Override                                               //TODO: Make do something
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.alumni_fragment_layout, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        this.inflatedView = inflater.inflate(R.layout.alumni_fragment_layout, container, false);
+
+        favoriteButton = inflatedView.findViewById(R.id.favoriteButton);
+
+        favoriteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(favoriteButton.getTag().equals("Off")){
+                    favoriteButton.setImageResource(R.drawable.ic_favorite_on);
+                    favoriteButton.setTag("On");
+                }
+                else if(favoriteButton.getTag().equals("On")){
+                    favoriteButton.setImageResource((R.drawable.ic_favorite_off));
+                    favoriteButton.setTag("Off");
+                }
+
+            }
+        });
+
+        return inflatedView;
     }
 
+    public void onViewCreated(View view, Bundle savedInstanceState){
+    }
 }
