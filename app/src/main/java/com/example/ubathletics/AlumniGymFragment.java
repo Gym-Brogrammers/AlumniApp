5b14d26d5addb6d6681ae3a6cfe3e7ff30ee9197
CJ <cmlong3@buffalo.edu>
2018-11-07 12:41:49 -0500

    Reformatted graph parameters to allow for graph to change data without reinstantiating. Allows for enter button to be handled, taking in date string. Split date up into ints, to be used to query data when that is ready

--- app/src/main/java/com/example/ubathletics/AlumniGymFragment.java
+++ app/src/main/java/com/example/ubathletics/AlumniGymFragment.java
@@ -10,19 +10,25 @@ import android.support.v4.app.FragmentTransaction;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Random;
 
 public class AlumniGymFragment extends Fragment {            //Sets up super basic fragment
     View inflatedView = null;
     ImageButton favoriteButton = null;
     EditText dateView = null;
+    boolean _isProper=false;
     Activity activity;
-
+    GraphFragment alumniGraph;
     @Override
     public void onAttach(Context context){
         super.onAttach(context);
@@ -74,15 +80,12 @@ public class AlumniGymFragment extends Fragment {            //Sets up super bas
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
                 afterChange=after;
             }
-
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 beforeChange=before;
             }
-
             @Override
             public void afterTextChanged(Editable s) {
-
                 if(((s.length()==3)||s.length()==6)&&beforeChange>afterChange){
                     s.delete(s.length()-1,s.length());
                 }
@@ -97,15 +100,24 @@ public class AlumniGymFragment extends Fragment {            //Sets up super bas
                     s.insert(5,"/");
                 }
 
-                if(s.length()>=3) {
-                    if((s.charAt(0) >= '2') || ((s.charAt(0) == '1') && (s.charAt(1) > '2'))) {
-                        s.delete(2,3);
-                        Toast toast = Toast.makeText(inflatedView.getContext(),"Please enter a proper date",Toast.LENGTH_LONG);
-                        toast.setGravity(Gravity.CENTER_VERTICAL|Gravity.CENTER_HORIZONTAL,0,-340);
+                if(s.length()>0) {
+                    if (s.charAt(s.length() - 1) == '.') {
+                        s.delete(s.length() - 1, s.length());
+                        Toast toast = Toast.makeText(inflatedView.getContext(), "Periods are not allowed", Toast.LENGTH_LONG);
+                        toast.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL, 0, -340);
                         toast.show();
                     }
                 }
-                if(s.length()>=6) {
+
+                if(s.length()==10){
+                    if((s.charAt(6)!='2')&&(s.charAt(7)!='0')&&(s.charAt(8)!='1')&&((s.charAt(9)<'7')||(s.charAt(9)>'9'))){
+                        Toast toast = Toast.makeText(inflatedView.getContext(),"Please enter date between last year and next year",Toast.LENGTH_LONG);
+                        toast.setGravity(Gravity.CENTER_VERTICAL|Gravity.CENTER_HORIZONTAL,0,-340);
+                        toast.show();
+                    }
+                    else{_isProper=true;}
+                }
+                else if(s.length()>=6) {
                     if ((s.charAt(3) >= '4') || ((s.charAt(3) == '3') && (s.charAt(4) >= '3'))) {
                         s.delete(5,6);
                         Toast toast = Toast.makeText(inflatedView.getContext(),"Please enter a proper date",Toast.LENGTH_LONG);
@@ -113,13 +125,39 @@ public class AlumniGymFragment extends Fragment {            //Sets up super bas
                         toast.show();
                     }
                 }
-                if(s.length()==10){
-                    if((s.charAt(6)!='2')&&(s.charAt(7)!='0')&&(s.charAt(8)!='1')&&((s.charAt(9)<'7')||(s.charAt(9)>'9'))){
-                        Toast toast = Toast.makeText(inflatedView.getContext(),"Please enter date between last year and next year",Toast.LENGTH_LONG);
+                else if(s.length()>=3) {
+                    if((s.charAt(0) >= '2') || ((s.charAt(0) == '1') && (s.charAt(1) > '2'))) {
+                        s.delete(2,3);
+                        Toast toast = Toast.makeText(inflatedView.getContext(),"Please enter a proper date",Toast.LENGTH_LONG);
                         toast.setGravity(Gravity.CENTER_VERTICAL|Gravity.CENTER_HORIZONTAL,0,-340);
                         toast.show();
                     }
                 }
+
+            }
+        });
+
+        dateView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId==EditorInfo.IME_ACTION_DONE) {
+                    //Get data from date given
+                    if(dateView.getText().length()==10) {
+                        String dateString = String.valueOf(dateView.getText());
+                        int month = Integer.parseInt(dateString.substring(0, 2));
+                        int day = Integer.parseInt(dateString.substring(3, 5));
+                        int year = Integer.parseInt(dateString.substring(6, 10));
+                    }
+
+                    int[] data = new int[25];
+                    Random rand = new Random();
+                    for(int i=0;i<25;i++){
+                        data[i]= rand.nextInt(80);
+                    }
+                    updateGraph(data);
+                    return true;
+                }
+                return false;
             }
         });
 
@@ -129,17 +167,25 @@ public class AlumniGymFragment extends Fragment {            //Sets up super bas
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        FragmentTransaction trans;
-        GraphFragment alumniGraph = new GraphFragment();
+
         int[] data = new int[25];
         data[4]=6;
 
+        FragmentTransaction trans;
+        alumniGraph = new GraphFragment();
         Bundle args = new Bundle();
         args.putIntArray("data",data);
         alumniGraph.setArguments(args);
         trans = getChildFragmentManager().beginTransaction();
         trans.add(R.id.alumni_graph_frame, alumniGraph);
         trans.commit();
+    }
+
+    public void updateGraph(int[] data){
+        Bundle args = new Bundle();
+        args.putIntArray("data",data);
+        alumniGraph.setArguments(args);
+        alumniGraph.updateGraph();
     }
 
 }
--- app/src/main/java/com/example/ubathletics/GraphFragment.java
+++ app/src/main/java/com/example/ubathletics/GraphFragment.java
@@ -6,32 +6,41 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.helper.StaticLabelsFormatter;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class GraphFragment extends Fragment {
     LineGraphSeries<DataPoint> series;
     int[]dataFromParent;
+    GraphView graph;
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        Bundle args = getArguments();
-        dataFromParent = args.getIntArray("data");
         return inflater.inflate(R.layout.graphview, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        GraphView graph = view.findViewById(R.id.graph);
+        graph = view.findViewById(R.id.graph);
         //graph.setScrollY(100);
         graph.setTitle("Projected Business Levels");
         //graph.getViewport().setScalable(true);
         graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setYAxisBoundsManual(true);
         graph.getViewport().setMinX(0);
-        graph.getViewport().setMaxX(24);
-        graph.getGridLabelRenderer().setNumHorizontalLabels(6);
+        graph.getViewport().setMaxX(25);
+        graph.getViewport().setMinY(0);
+        graph.getViewport().setMaxY(80);
+
+        updateGraph();
+    }
+    public void updateGraph(){
+        graph.removeAllSeries();
+        dataFromParent = getArguments().getIntArray("data");
         series = new LineGraphSeries<>();
         for(int i=0;i<25;i++) {
             series.appendData(new DataPoint(i, dataFromParent[i]), true, 24);
         }
         graph.addSeries(series);
-    }}
+    }
+}
